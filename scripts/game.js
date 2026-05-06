60ae6b1407cbc54ce46e7e6d1891c6db4b289092
midej0 <jovan.ds.miljkovic@gmail.com>
2026-05-06 09:20:01 +0200

    fixed glitch with hold note timeHeld counter not resetting after successfully hitting the end note

--- scripts/game.js
+++ scripts/game.js
@@ -476,6 +476,7 @@ function Input(lane) {
                 if (!note.scored) {
                     shouldCount[note.lane] = true;
                     ChangeGrade(GetScore(Math.abs(leastTimeDifference)));
+                    timeHeld[note.lane] = 0;
                     note.scored = true;
                 }
                 break;
